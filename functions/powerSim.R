ef73ecc2ef7108c3373ba84106309d7b744829dc
LeonardoJost <leo1989@gmx.de>
2023-09-14 12:23:53 +0200

    added adjustment factor of effect size for number of measurements

--- functions/powerSim.R
+++ functions/powerSim.R
@@ -33,7 +33,7 @@ generateData=function(n,rmCorrelation=0.66,totalSd=1) {
   testdata$baseMean=ifelse(testdata$factor1=="typeA" & testdata$factor2=="type1" ,0,
                     ifelse(testdata$factor1=="typeB" & testdata$factor2=="type1" ,0,
                     ifelse(testdata$factor1=="typeA" & testdata$factor2=="type2" ,0,
-                    ifelse(testdata$factor1=="typeB" & testdata$factor2=="type2" ,0.8, #cohens d 0.4 for both main effects and the interaction
+                    ifelse(testdata$factor1=="typeB" & testdata$factor2=="type2" ,0.8/sqrt(2), #cohens d 0.4 for both main effects and the interaction
                     0))))
   #convert to numeric to test for main effects as part of interactions
   testdata$factor1Numeric=sapply(testdata$factor1,function(i) contr.sum(2)[i,])*0.5
@@ -101,8 +101,8 @@ ns=c(41)
 significantDataFrame=randSim(ns,rmCorrelation=0.5,totalSd=1)
 significantDataFrame2=randSim(ns,rmCorrelation=0.66,totalSd=1)
 
-#power calculation using superpower
-mu = c(0,0,0,0.8) #at sd of 1 this should equal cohens d of 0.4
+#power calculation using superpower (divide by sqrt(2) to account for half the number of measurements compared with only 2w factor)
+mu = c(0,0,0,0.8/sqrt(2)) #at sd of 1 this should equal cohens d of 0.4
 n = 41 #for comparisons with GPower and between effects: here: n per group, GPower: total N
 sd = 1 
 r = .66
@@ -122,4 +122,86 @@ exact_result = ANOVA_exact(design_result,
                            verbose=T)
 exact_result$main_results
 
+#get effect size for power of .8
+mu = c(0,0,0,0.66/sqrt(2)) # d =.33
+n = 41 #for comparisons with GPower and between effects: here: n per group, GPower: total N
+sd = 1 
+r = .66
+string = "2w*2w"
+alpha_level = 0.1 #double alpha level because of directed effects/analysis
+labelnames = c("condition","mr","control","step","1","2")
+design_result = ANOVA_design(design = string,
+                             n = n, 
+                             mu = mu, 
+                             sd = sd, 
+                             r = r,
+                             labelnames = labelnames,
+                             plot=F)
+design_result
+exact_result = ANOVA_exact(design_result,
+                           alpha_level = alpha_level,
+                           verbose=T)
+exact_result$main_results
 
+#comparison: original power analysis for within-subjects effects and two groups
+mu = c(0,0.4) #at sd of 1 this should equal cohens d of 0.4
+n = 41 #for comparisons with GPower and between effects: here: n per group, GPower: total N
+sd = 1 
+r = .5
+string = "2w"
+alpha_level = 0.1 #double alpha level because of directed effects/analysis
+labelnames = c("condition","mr","control")
+design_result = ANOVA_design(design = string,
+                             n = n, 
+                             mu = mu, 
+                             sd = sd, 
+                             r = r,
+                             labelnames = labelnames,
+                             plot=F)
+design_result
+exact_result = ANOVA_exact(design_result,
+                           alpha_level = alpha_level,
+                           verbose=T)
+exact_result$main_results
+
+#comparison: two within-subjects effects with the same power 
+mu = c(0,0,0,0.8) #at sd of 1 this should equal cohens d of 0.4
+n = 21 #for comparisons with GPower and between effects: here: n per group, GPower: total N
+sd = 1 
+r = .5
+string = "2w*2w"
+alpha_level = 0.1 #double alpha level because of directed effects/analysis
+labelnames = c("condition","mr","control","step","1","2")
+design_result = ANOVA_design(design = string,
+                             n = n, 
+                             mu = mu, 
+                             sd = sd, 
+                             r = r,
+                             labelnames = labelnames,
+                             plot=F)
+design_result
+exact_result = ANOVA_exact(design_result,
+                           alpha_level = alpha_level,
+                           verbose=T)
+exact_result$main_results
+
+#comparison: divide effect size by sqrt(2) instead to account for fewer measurements
+mu = c(0,0,0,0.8/sqrt(2)) #at sd of 1 this should equal cohens d of 0.4
+n = 41 #for comparisons with GPower and between effects: here: n per group, GPower: total N
+sd = 1 
+r = .5
+string = "2w*2w"
+alpha_level = 0.1 #double alpha level because of directed effects/analysis
+labelnames = c("condition","mr","control","step","1","2")
+design_result = ANOVA_design(design = string,
+                             n = n, 
+                             mu = mu, 
+                             sd = sd, 
+                             r = r,
+                             labelnames = labelnames,
+                             plot=F)
+design_result
+exact_result = ANOVA_exact(design_result,
+                           alpha_level = alpha_level,
+                           verbose=T)
+exact_result$main_results
